5bcad8da9c7e1eaf9b21934478a6c878de22c25d
Lily <1368025+Lily418@users.noreply.github.com>
2025-02-15 23:59:34 +0000

    Remove new lines from json

--- commands/echo.ts
+++ commands/echo.ts
@@ -10,7 +10,7 @@ export default class Echo extends BaseCommand {
   static options: CommandOptions = {}
 
   async run() {
-    const document = fs.readFileSync('create-hello-world.json', 'utf-8')
+    const document = fs.readFileSync('create-hello-world.json', 'utf-8').split('\n').join('').trim()
     const privateKey = fs.readFileSync('keys/private.pem', 'utf-8').split('\n').join('').trim()
 
     const digest = 'SHA-256=' + createHash('sha256').update(document).digest('base64')
