d2fe346e7dc6517c8e2113888daa191177c87765
Lily <1368025+Lily418@users.noreply.github.com>
2025-02-16 20:38:45 +0000

    Hardening the security, checking the date header, ensuring our comparison string contains certain headers

--- start/routes.ts
+++ start/routes.ts
@@ -9,7 +9,7 @@
 
 import logger from '@adonisjs/core/services/logger'
 import router from '@adonisjs/core/services/router'
-import { verify } from 'node:crypto'
+import { createHash, verify } from 'node:crypto'
 import fs from 'node:fs'
 import { signedRequest } from '../utils/sign_request.js'
 
@@ -51,13 +51,55 @@ router.get('/.well-known/webfinger', async () => {
 
 router.post('/inbox', async ({ request, response }) => {
   inboxLogger.info(`Request to /inbox from ip: ${request.ip()}`)
-  inboxLogger.debug(request.body(), 'Request Body')
-  inboxLogger.debug(request.headers(), 'Request Headers')
+  inboxLogger.info(request.body(), 'Request Body')
+  inboxLogger.info(request.headers(), 'Request Headers')
+
+  const rawBody = request.raw()
+  if (!rawBody) {
+    inboxLogger.info('Missing raw body')
+    return response.status(401).send({ error: 'Expected a request body' })
+  }
+
+  const dateHeader = request.header('Date')
+  if (!dateHeader) {
+    inboxLogger.info('Disregarding inbox message for Missing Date Header')
+    return response.status(401).send({ error: 'Missing Date Header' })
+  }
+
+  const parsedDateInHeader = Date.parse(dateHeader)
+  const currentTime = new Date()
+
+  if (Number.isNaN(parsedDateInHeader)) {
+    return response
+      .status(401)
+      .send({ error: `Date Header Could Not Be Parsed ${parsedDateInHeader}` })
+  }
+  if (
+    parsedDateInHeader < currentTime.setMinutes(currentTime.getMinutes() - 1) ||
+    parsedDateInHeader > currentTime.setMinutes(currentTime.getMinutes() + 1)
+  ) {
+    inboxLogger.info(
+      {
+        dateHeader: new Date(parsedDateInHeader).toUTCString(),
+        currentTime: currentTime.toUTCString(),
+      },
+      'Disregarding inbox message for Missing Date Header'
+    )
+
+    return response.status(401).send({
+      error: `Date Header Not Within Minute of Server Time.`,
+      dateHeader: new Date(parsedDateInHeader).toUTCString(),
+      currentTime: currentTime.toUTCString(),
+    })
+  }
+
+  const digestHeader = request.header('Digest')
+  if (!digestHeader) {
+    inboxLogger.info('Disregarding inbox message for missing digest')
+    return response.status(401).send({ error: `Digest Header Not Found` })
+  }
 
   // TODO: We are missing security checks which would be required in a serious production application
-  // For example:
-  // "The request contains a Date header. Compare it with current date and time within a reasonable time window to prevent replay attacks.
-  // It is advisable that requests with payloads in the body also send a Digest header, and that header be signed along in the signature. If it’s present, it should be checked as another special case within the comparison string: Instead of taking the digest value from the received header, recompute it from the received body.
   // While this proves the request comes from an actor, what if the payload contains an attribution to someone else? In reality you’d want to check that both are the same, otherwise one actor could forge messages from other people."
   // See https://blog.joinmastodon.org/2018/07/how-to-make-friends-and-verify-requests/
   const signatureHeader = request.header('Signature')
@@ -128,17 +170,50 @@ router.post('/inbox', async ({ request, response }) => {
     inboxLogger.info('Key id response did not contain public key')
     return response.status(401).send({ error: `Failed to fetch fetch ${keyId}` })
   }
+  const expectedDigest = `SHA-256=${createHash('sha256').update(rawBody).digest('base64')}`
+
+  // We are checking for the presence of mandatory headers in the comparison string we generate, ideally we would do this in a more structured way
+  // which checks that any headers we use are included in the signature.
+  const seenHeadersInSignature = {
+    date: false,
+    host: false,
+    digest: false,
+    requestTarget: false,
+  }
 
   const comparisonString = headers
     .split(' ')
     .map((signedHeaderName) => {
       if (signedHeaderName === '(request-target)') {
+        seenHeadersInSignature.requestTarget = true
         return '(request-target): post /inbox'
+      } else if (signedHeaderName === 'digest') {
+        seenHeadersInSignature.digest = true
+        return `digest: ${expectedDigest}`
       } else {
+        if (signedHeaderName === 'date') {
+          seenHeadersInSignature.date = true
+        }
+
+        if (signedHeaderName === 'host') {
+          seenHeadersInSignature.host = true
+        }
+
         return `${signedHeaderName}: ${request.header(String(signedHeaderName).charAt(0).toUpperCase() + String(signedHeaderName).slice(1))}`
       }
     })
     .join('\n')
+
+  const anyRequiredHeadersMissingInSignature = Object.values(seenHeadersInSignature).some(
+    (v) => v !== true
+  )
+
+  if (anyRequiredHeadersMissingInSignature) {
+    inboxLogger.info(seenHeadersInSignature, 'Missing Headers In Signature')
+    return response.status(401).send({
+      error: `Signature is missing one or more required headers`,
+    })
+  }
 
   inboxLogger.info(comparisonString, 'We are using the comparison string:')
 
@@ -153,7 +228,9 @@ router.post('/inbox', async ({ request, response }) => {
     })
   }
 
-  inboxLogger.info(request.body(), 'We have validated the message')
+  const requestBody = request.body()
+
+  inboxLogger.info(requestBody, 'We have validated the message')
   return {}
 })
 
