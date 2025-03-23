2099fa2ecec4ff48190e4bec10970f0dc9d2c182
Lily <1368025+Lily418@users.noreply.github.com>
2025-03-23 15:05:27 +0000

    Auto increment id

--- database/migrations/1740265321213_create_actors_table.ts
+++ database/migrations/1740265321213_create_actors_table.ts
@@ -5,7 +5,7 @@ export default class extends BaseSchema {
 
   async up() {
     this.schema.createTable(this.tableName, (table) => {
-      table.string('id', 2000)
+      table.increments('id').notNullable()
       table.primary(['id'])
 
       table.timestamp('created_at')
